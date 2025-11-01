1ef53ff57ae1cc8441ce499e8f96a91319cf5e9e
anancds123 <anancds@163.com>
2025-11-01 21:32:30 +0800

    feat: fixed union type
    Signed-off-by: anancds123 <anancds@163.com>

--- hyperlangExtension/tests/cases/unionTypes1.d.ts
+++ hyperlangExtension/tests/cases/unionTypes1.d.ts
@@ -4,9 +4,19 @@
 //
 // See https://cangjie-lang.cn/pages/LICENSE for license information.
 
-package my_module
+// type alias
+type a1 = string|number
 
-import ohos.ark_interop.*
-import ohos.ark_interop_helper.*
-import ohos.base.*
+// global func
+export function test(a:a1) : string|number
 
+export class A {
+  a:a1
+  // property
+  b: boolean | string
+
+  // member function
+  test1(a:a1) : string|number
+
+  test2(a: string|number): number
+}
